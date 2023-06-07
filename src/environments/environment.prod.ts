5596dfcc72e2f777925f39c50603ff7687de2ec4
Juan Jose Melendez <proyectos.jjcorp@gmail.com>
2023-06-06 22:07:24 -0600

    Modificacion de environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
   servidor: {
-
-    //TAG_SERVIDOR: 'http://www.jjcorp.com.mx/TopMkt',
-    //TAG_SERVIDOR_W: 'http://www.jjcorp.com.mx/TopMas',
     TAG_IDEMPRESA: 0,
     TAG_SERVIDOR: 'https://www.topmas.mx/TopMas',
     TAG_SERVIDOR_W: 'https://www.topmas.mx',
